c9fc9ff21bd26b6b75d0843841d3ce9fc529d19d
justytho <justinmthomp@gmail.com>
2021-06-03 21:22:38 -0400

    Comments from LiveShare

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,24 +1,29 @@
 import { useState } from 'react'
-
-const AddTodo = ({ onAdd }) => { 
+// Declare AddTodo component and pass onAdd prop
+const AddTodo = ({ onAdd }) => {
+    //declare an empty string as the initial state, get the returned initial state, item, and a function, setItem, for future update
     const [item, setItem] = useState('')
-
+    // when submit button is clicked, todo item from form is passed to onAdd method.
     const onSubmit = (e) => {
+        // PreventDefault is called to prevent the browser from reloading/refreshing before the todo is added
         e.preventDefault()
+        //add todo item
         onAdd({item})
-
+        //manually reset field to blank
         setItem('')
     }
 
     return (
+        //onSubmit function will be called while the form is submitted
         <form onSubmit={onSubmit}>
             <label>Add Todo:</label>
-            <input 
-                type="text" 
+            {/* The input value is set to be item, which is an empty string initially. The new item inserted is updated through the setItem function.*/}
+            <input
+                type="text"
                 placeholder="Todo Item"
                 value={item}
                 onChange={ (e) => setItem(e.target.value)}
-            /> 
+            />
             <input type="submit" value="Add Todo" />
         </form>
     )
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,14 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getCLS(onPerfEntry); // CLS Cumulative Layout Shift - measures visual stability (checks for any unexpected layout shifts) Layout shift: occurs when a visible element changes position or size and then affects the position of content around it.
+      getFID(onPerfEntry); // FID First Input Delay - measures load responsiveness
+      getFCP(onPerfEntry); // FCP First Contentful Paint - measures perceived load speed (marks the first point in the page load timeline when user sees something on screen)
+      getLCP(onPerfEntry); // LCP Largest Contentful Paint  - measures perceived load speed (marks the point in the page load when the page's main content has likely loaded)
+      getTTFB(onPerfEntry); // TTFB Time to First Byte - time that it takes for a user's browser to receive the first byte of page content
     });
   }
 };
 
+// export reportWebVitals
 export default reportWebVitals;
